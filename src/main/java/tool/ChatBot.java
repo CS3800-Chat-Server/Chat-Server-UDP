f4504b38a9625c8e49320373ac98e2942b539841
Alexander Rodriguez <rodriguez.al183@gmail.com>
2023-05-12 22:45:09 -0700

    edited chat bot

--- src/main/java/tool/ChatBot.java
+++ src/main/java/tool/ChatBot.java
@@ -9,6 +9,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -24,11 +25,13 @@ public class ChatBot implements Runnable {
     InetSocketAddress addr;
     private volatile boolean isRunning = true;
     private int id;
+    private String username;
 
     public ChatBot(String ip, int port, int id) {
         addr = new InetSocketAddress(ip, port);
         try {
             this.socket = new DatagramSocket(277 + id);
+            socket.setSoTimeout(2000);
             this.id = id;
         } catch (SocketException e) {
             System.err.println("ChatBot() Error: " + e.getMessage());
@@ -38,28 +41,35 @@ public class ChatBot implements Runnable {
 
     @Override
     public void run() {
-        String username = "Bot " + id;
+        username = "Bot " + id;
+        String loginString = "LOGIN " + username;
 
-        byte[] buff = username.getBytes();
+        byte[] buff = loginString.getBytes();
         DatagramPacket loginPacket = new DatagramPacket(buff, buff.length, addr);
 
         try {
+            System.out.println("Loggin in " + username);
             socket.send(loginPacket);
 
             buff = new byte[1024];
             DatagramPacket usersPacket = new DatagramPacket(buff, buff.length);
 
+            System.out.println(username + " waiting for user list");
             socket.receive(usersPacket);
 
             String userList = new String(usersPacket.getData(), 0, usersPacket.getLength());
             String[] tokens = userList.split(" ");
 
+            System.out.println(username + " received user list : " + userList);
+
             for (String s : tokens) {
                 if (!s.equals("USERS")) {
                     String[] userInfo = s.split(":");
-                    peers.put(userInfo[0], Integer.parseInt(userInfo[1]));
+                    peers.put(userInfo[0].split("/")[0], Integer.parseInt(userInfo[1]));
                 }
             }
+
+            System.out.println(username + " logged in");
 
             BotOutput botOutput = new BotOutput();
             new Thread(botOutput).start();
@@ -68,14 +78,23 @@ public class ChatBot implements Runnable {
                 buff = new byte[1024];
                 DatagramPacket messagePacket = new DatagramPacket(buff, buff.length);
 
-                socket.receive(messagePacket);
+                System.out.println(username + " waiting for message");
+                try {
+                    socket.receive(messagePacket);
+                } catch (SocketTimeoutException e) {
+                    //System.out.println(username + " timed out waiting for message, moving on");
+                    continue;
+                }                
 
                 String message = new String(messagePacket.getData(), 0, messagePacket.getLength());
                 String[] messageTokens = message.split(" ");
 
+                System.out.println(username + " received message\t" + message);
+
                 if (messageTokens[0].equals("MESSAGE")) {
                     System.out.println(message);
                 } else if (messageTokens[0].equals("LOGIN")) {
+                    System.out.println(username + " adding new client to map");
                     String[] userInfo2 = messageTokens[2].split(":");
                     peers.put(userInfo2[0], Integer.parseInt(userInfo2[1]));
                 } else if (messageTokens[0].equals("LOGOFF")) {
@@ -84,10 +103,14 @@ public class ChatBot implements Runnable {
                 }
             }
 
-            String logoffString = "LOGOUT " + username;
+            System.out.println("Logging out " + username);
+
+            String logoffString = "LOGOFF " + username;
             byte[] buffer = logoffString.getBytes();
             DatagramPacket logoffPacket = new DatagramPacket(buffer, buffer.length, addr);
             socket.send(logoffPacket);
+
+            System.out.println(username + " logged out");
 
             socket.close();
 
@@ -108,9 +131,10 @@ public class ChatBot implements Runnable {
         @Override
         public void run() {
             while (isRunning) {
-                String output = "This is a message from bot " + id;
+                String output = "MESSAGE This is a message from bot " + id;
                 byte[] buffer = output.getBytes();
                 for (HashMap.Entry<String, Integer> pair : peers.entrySet()) {
+                    System.out.println(username + " sending message to " + pair.getKey() + ":" + pair.getValue());
                     InetSocketAddress peerAddr = new InetSocketAddress(pair.getKey(), pair.getValue());
                     DatagramPacket packet = new DatagramPacket(buffer, buffer.length, peerAddr);
                     try {
